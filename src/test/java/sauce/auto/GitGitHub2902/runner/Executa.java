cfc6151534b27cbfa8f2367e5e2ef076c4516388
ntwashington <washington.tn7@gmail.com>
2024-04-16 00:23:29 -0300

    inclusão da class Executa

---
+++ src/test/java/sauce/auto/GitGitHub2902/runner/Executa.java
@@ -0,0 +1,13 @@
+package sauce.auto.GitGitHub2902.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions()
+
+public class Executa {
+
+}
